210e09c8eea832a90ef3caa0bb422a998edc8bc0
alex.cera <alex.cera@est.fib.upc.edu>
2020-03-12 11:41:04 +0100

    prueba3

--- src/main/java/io/github/ceraalex99/petandgo/Main.java
+++ src/main/java/io/github/ceraalex99/petandgo/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
 
 
-    public int sum(int a, int b){
+    public int sum (int a, int b)
+    {
         return a + b;
     }
 
-    public int mult(int a, int b){
-        return a * b;
+    public int mult(int a,int b){
+        return a*b;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) { System.out.println("Hello world");
         System.out.print("hola");
     }
 }
