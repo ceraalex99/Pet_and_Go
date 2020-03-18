ced5cef796532ea5f3522a583142111402a8272d
alex.cera <alex.cera@est.fib.upc.edu>
2020-03-18 19:41:12 +0100

    error hibernateconfig

--- src/main/java/hibernate/HibernateConfig.java
+++ src/main/java/hibernate/HibernateConfig.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class HibernateConfig{
     static File F;
     public HibernateConfig(){
-        if (F == null) F  = new File("src\\hibernate.cfg.xml");
+        if (F == null) F  = new File("src/hibernate.cfg.xml");
     }
     public static File getConfigFile() {
         return F;
