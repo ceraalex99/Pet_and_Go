e987f2138e529249c60c07a625cb6f46cbdaf4ad
ceraalex99 <ceraalex99@gmail.com>
2020-04-16 13:43:09 +0200

    quitar import sin usar

--- src/main/java/api/controller/UsuarioController.java
+++ src/main/java/api/controller/UsuarioController.java
@@ -8,7 +8,6 @@ import api.services.UsuarioServices;
 import entities.Mascota;
 import entities.MascotaId;
 import entities.Usuario;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
