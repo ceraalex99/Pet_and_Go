6963940437bb508f6fc12ec4fc37125a3ce5cb9d
alex.cera <alex.cera@est.fib.upc.edu>
2020-04-21 16:22:14 +0200

    procfile

--- src/main/java/api/controller/UsuarioController.java
+++ src/main/java/api/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.security.NoSuchAlgorithmException; 
+import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 
