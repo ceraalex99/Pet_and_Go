54b2644398be96077b77741f9d4d0f7ad440f10a
lbelloso <luisbelloso87@gmail.com>
2020-04-23 22:19:40 +0200

    prueba

--- src/main/java/api/controller/MainController.java
+++ src/main/java/api/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainController {
     public static void main(String[] args) {
         SpringApplication.run(MainController.class, args);
+
     }
 
 }
