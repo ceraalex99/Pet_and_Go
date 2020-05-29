7f92676f9dafeb783d3e363f52f3e7b40ff3c97d
lbelloso <luisbelloso87@gmail.com>
2020-05-29 02:07:16 +0200

    add thread actualizador de nivel cada 30 min

--- src/main/java/api/controller/MainController.java
+++ src/main/java/api/controller/MainController.java
@@ -1,8 +1,7 @@
 package api.controller;
 
 import helpers.ActualizadorPuntos;
-import helpers.Lanzador;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
